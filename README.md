c86795c708ae304b1383e67ddfc8603e7d982851
image <337067051@qq.com>
2018-08-06 20:07:25 +0800

    Seq2Seq baseline

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ﻿# Pangyunsheng
 Seq2Seq文件夹下的模型是第一个版本的seq2seq，只加了Attention，且数据集不是rap数据集  
 RapGenerator文件夹下的模型是第二版的seq2seq，在第一版seq2seq基础上加入了beam serach，加入了GRUCell作为可选的cell_type，数据集换成了rap数据集
+RapGeneratorV2文件夹下的模型是第三版的seq2seq
+
+Seq2Seq（终极版）是最终的Seq2Seq model，以后的改进都基于这个版本的Seq2Seq
---
+++ Seq2Seq（终极版）/predict.py
@@ -0,0 +1,82 @@
+# ！/usr/bin/env python
+#  _*_ coding:utf-8 _*_
+
+import tensorflow as tf
+import numpy as np
+
+# from Seq2Seq.HyperParameter import HyperParameter
+from data_helpers import *
+from model import Seq2SeqModel
+import sys
+
+
+def predict_ids_to_seq(predict_ids, id2word, beam_size):
+    """
+    将beam_search返回的结果转化为字符串
+    :param predict_ids: 列表，长度为batch_size，每个元素都是decode_len*beam_size的数组
+    :param id2word: vocab字典
+    :return:
+    """
+    for single_predict in predict_ids:
+        for i in range(beam_size):
+            print("Beam search result {}：".format(i + 1))
+            predict_list = np.ndarray.tolist(single_predict[:, :, i])
+            predict_seq = [id2word[idx] for idx in predict_list[0]]
+            print(" ".join(predict_seq))
+            print()
+
+
+if __name__ == '__main__':
+    # 超参数
+    hp = HyperParameter()
+    rnn_size = hp.rnn_size
+    num_layers = hp.num_layers
+    embedding_size = hp.embedding_size
+    batch_size = hp.batch_size
+    learning_rate = hp.learning_rate
+    epochs = hp.epochs
+    steps_per_checkpoint = hp.steps_per_checkpoint
+    sources_txt = hp.sources_txt
+    targets_txt = hp.targets_txt
+    model_dir = hp.model_dir
+    beam_size = hp.beam_size
+
+    # 得到分词后的sources和targets
+    sources = load_and_cut_data(sources_txt)
+    targets = load_and_cut_data(targets_txt)
+
+    # 根据sources和targets创建词典，并映射
+    sources_data, targets_data, word_to_id, id_to_word = create_dic_and_map(sources, targets)
+
+    with tf.Session() as sess:
+        model = Seq2SeqModel(
+            sess=sess,
+            rnn_size=rnn_size,
+            num_layers=num_layers,
+            embedding_size=embedding_size,
+            word_to_id=word_to_id,
+            mode='predict',
+            learning_rate=learning_rate,
+            use_attention=True,
+            beam_search=True,
+            beam_size=beam_size,
+            cell_type='LSTM',
+            max_gradient_norm=5.0
+        )
+        ckpt = tf.train.get_checkpoint_state(model_dir)
+        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+            print('Reloading model parameters..')
+            model.saver.recover_last_checkpoints(ckpt.all_model_checkpoint_paths)
+            model.saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            raise ValueError('No such file:[{}]'.format(model_dir))
+
+        sys.stdout.write("> ")
+        sys.stdout.flush()
+        sentence = sys.stdin.readline()
+        while sentence:
+            batch = sentence2enco(sentence, word_to_id)
+            predicted_ids = model.infer(batch)
+            predict_ids_to_seq(predicted_ids, id_to_word, beam_size)
+            sys.stdout.flush()
+            sentence = input("> ")
